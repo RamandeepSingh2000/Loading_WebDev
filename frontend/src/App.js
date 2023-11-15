e68d9ba291e5c58dcd24f7fb6e2d4961a01ce70d
Tuong Quyen Dao <daotuongquyen@gmail.com>
2023-11-15 14:11:27 -0500

    Upload Finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import GameUploadForm from './components/upload.component';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p> Landing page </p>
       </header>
+      <GameUploadForm />
     </div>
   );
 }
