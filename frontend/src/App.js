2babe7a7faee654ebee4c44bd914fad65fd8ddc4
Deinma Iyalla <buyoiyaz66@gmail.com>
2023-11-15 14:54:48 -0500

    Added the game info page and added its to route to the app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 
+import React from 'react';
+import{ BrowserRouter, Routes, Route } from 'react-router-dom';
+import GameInfoPage from './GameInfoPage';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p> Landing page </p>
+        
       </header>
+
+      <div> 
+        <BrowserRouter>
+        
+        <Routes>
+                 
+                  <Route path='/game-info' element={ <GameInfoPage/>}/>
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+
     </div>
   );
 }
