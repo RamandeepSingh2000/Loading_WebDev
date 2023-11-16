2cd95b092c02fe4a333cf25a37c240f10a1331d3
Ramandeep Singh <rs1220904@gmail.com>
2023-11-16 17:00:54 -0500

    integration

--- frontend/src/components/gamesgrid.component.js
+++ frontend/src/components/gamesgrid.component.js
@@ -1,16 +1,39 @@
 import React, {Component} from "react";
 import ItemCard from "./itemcard.component";
+import axios from "axios";
 
 export default class GamesGrid extends Component{
   constructor(props){
     super(props)
-
+    this.state = {games: []}
   }
+
+  componentDidMount(){
+      
+      axios.get('http://localhost:8081/api/games')
+      .then(res => this.setState({games: res.data}))
+      .catch(e => console.log(e));
+  }
+
+  componentDidUpdate(){
+      axios.get('http://localhost:8081/api/games')
+      .then(res => this.setState({games: res.data}))
+      .catch(e => console.log(e));
+  }
+
+  gamesList()
+  {
+      return this.state.games.map(function(currentGame, i){
+          return <ItemCard game={currentGame} key={i} />
+      })
+  }
+
   render(){
     return(
       <div class="vagetables_section layout_padding margin_bottom90">
       <div class="courses_section_2">
-        <div class="row">
+        {this.gamesList()}
+        {/* <div class="row">
           <ItemCard/>
           <ItemCard/>
           <ItemCard/>
@@ -30,7 +53,7 @@ export default class GamesGrid extends Component{
           <ItemCard/>
           <ItemCard/>
           <ItemCard/>
-        </div>
+        </div> */}
       </div>
       </div>
     )
