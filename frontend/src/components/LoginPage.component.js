d05571a6fcfcc30913c1d2acbc774e3489d0ef9c
ainyafzaldev <ainyafzaldev@gmail.com>
2023-11-29 14:40:37 -0500

    input validation

--- frontend/src/components/LoginPage.component.js
+++ frontend/src/components/LoginPage.component.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
 function LoginPage() {
+  
   const [userInfo, setUserInfo] = useState({
     name: '',
     email: '',
--- frontend/src/components/RegisterPage.component.js
+++ frontend/src/components/RegisterPage.component.js
@@ -1,19 +1,62 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate  } from 'react-router-dom'
 import axios from 'axios';
 
 function RegisterPage() {
+  const USER_REGEX = new RegExp(/^[A-z][A-z0-9-_]{3,23}$/);
+  const EMAIL_REGEX = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/);
+  const PWD_REGEX = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/);
   const [userInfo, setUserInfo] = useState({
     name: '',
     email: '',
     password: ''
   });
+  const [validPwd, setValidPwd] = useState(false);
+  const [validName, setValidName] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(userInfo.name));
+  }, [userInfo.name]);
+  
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(userInfo.password));
+  }, [userInfo.password]);
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(userInfo.email));
+  }, [userInfo.email]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [userInfo.name, userInfo.password, userInfo.email]);
+
+  
   let navigate = useNavigate(); 
   const handleInputChange = (e) => {
     setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
   };
   const handleRegisterSubmit = async (e) => {
     e.preventDefault();
+    const v1 = USER_REGEX.test(userInfo.name);
+    const v2 = PWD_REGEX.test(userInfo.password);
+    const v3 = PWD_REGEX.test(userInfo.email);
+    console.log(v1);
+    if (!v1) {
+      setErrMsg("Invalid Username. Must be 4 to 24 characters and begin with a letter. " +
+      "Letters, numbers, underscores and hyphens are allowed.");
+      return;
+    }
+    if (!v3) {
+      setErrMsg("Invalid Email");
+      return;
+    }
+    if (!v2) {
+      setErrMsg("Invalid Password. Must be 8 to 24 characters and include uppercase and lowercase letters," +
+      " a number and a special character.");
+      return;
+    }
+  
 
     let formData = new FormData();
 
@@ -72,6 +115,8 @@ function RegisterPage() {
           className="form-control"
           id="name"
           name="name"
+          aria-invalid={validName ? "false" : "true"}
+          required
           value={userInfo.name}
           onChange={handleInputChange}
         />
@@ -83,6 +128,8 @@ function RegisterPage() {
           className="form-control"
           id="email"
           name="email"
+          aria-invalid={validEmail ? "false" : "true"}
+          required
           value={userInfo.email}
           onChange={handleInputChange}
         />
@@ -94,10 +141,13 @@ function RegisterPage() {
           className="form-control"
           id="password"
           name="password"
+          aria-invalid={validPwd ? "false" : "true"}
+          required
           value={userInfo.password}
           onChange={handleInputChange}
         />
       </div>
+      <p>{errMsg != "" ? errMsg: ""}</p>
 
       <button type="submit" className="btn btn-primary">
         Register
